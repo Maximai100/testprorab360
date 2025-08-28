a1df52ab1666ba4154269fe85d6ca6b5de416573
Maxim <you@example.com>
2025-08-28 21:37:53 +0300

    Feat: Implement ESLint and Prettier for code quality and consistency

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint'],
+  rules: {
+    // Add custom rules here
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'react/prop-types': 'off',
+  },
+};
